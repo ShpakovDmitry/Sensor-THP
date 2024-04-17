9db72537cea3595b808b88d6ac9823a57260d22b
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2024-04-17 22:53:41 +0300

    +) application: initial add

---
+++ firmware/application/application.hpp
@@ -0,0 +1,36 @@
+/* -*-c++-*-
+ * @file: application.hpp
+ * @author: Dmitry Shpakov
+ * @date: 2024/04/17
+ * @licence: LGPL3 (see text at the end of a file)
+ * @description: Application class definition
+ */
+
+#pragma once
+
+class Application {
+public:
+  void run();
+private:
+
+}; 
+
+/*
+
+Copyright © 2024 Dmitry Shpakov
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
